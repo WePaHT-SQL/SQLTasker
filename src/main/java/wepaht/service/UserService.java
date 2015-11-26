ba793fe86339df6e794eaecf7c601df03db2750a
Matti Räty <matti.raty@helsinki.fi>
2015-11-26 14:47:38 +0200

    user service: authenticated user can be found

---
+++ src/main/java/wepaht/service/UserService.java
@@ -0,0 +1,20 @@
+package wepaht.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import wepaht.domain.User;
+import wepaht.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByUsername(authentication.getName());
+    }
+}
