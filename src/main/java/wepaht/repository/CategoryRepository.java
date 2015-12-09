76bdc392468ec6cc2cfa289831bd1ffad580526e
Tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-12-09 20:43:48 +0200

    category starDate

--- src/main/java/wepaht/repository/CategoryRepository.java
+++ src/main/java/wepaht/repository/CategoryRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import wepaht.domain.Category;
 
 
+import java.util.Date;
 import java.util.List;
 
 @RestResource(exported = false)
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 
+    List<Category> findByStartDateBefore(Date date);
 
+    List<Category> findByStartDate(Date date);
 }
