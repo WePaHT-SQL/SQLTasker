afe42b4a94b08026aabe5919aa98fb730bb34822
Tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-12-21 17:09:24 +0200

    Task deletion works

--- src/main/java/wepaht/service/CategoryService.java
+++ src/main/java/wepaht/service/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import wepaht.domain.Category;
 import wepaht.domain.Task;
 import wepaht.repository.CategoryRepository;
+import wepaht.repository.TaskRepository;
 
 import java.util.List;
 
@@ -17,10 +18,14 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     /**
-     *  Adds task to categorys' task list.
+     * Adds task to categorys' task list.
+     *
      * @param categoryId which category
-     * @param task which task
+     * @param task       which task
      */
     @Transactional
     public void setCategoryToTask(Long categoryId, Task task) {
@@ -29,7 +34,24 @@ public class CategoryService {
         if (!taskList.contains(task)) {
             taskList.add(task);
             category.setTaskList(taskList);
+            task.addCategoryToList(category);
+            taskRepository.save(task);
         }
     }
+
+    public void removeTaskFromCategory(Task task) {
+        List<Category> categoryList =task.getCategoryList();
+        for (Category category : categoryList) {
+            List<Task> taskList = category.getTaskList();
+            taskList.remove(task);
+            category.setTaskList(taskList);
+            categoryRepository.save(category);
+        }
+
+        categoryList.clear();
+        task.setCategoryList(categoryList);
+        taskRepository.save(task);
+    }
+
 
 }
