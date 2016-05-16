1514451babc1c54fad3142e5069eb2abfecda312
ArktinenSieni <matti.raty@helsinki.fi>
2016-05-16 15:31:41 +0300

    urr

--- src/main/java/wepaht/profile/ProdProfile.java
+++ src/main/java/wepaht/profile/ProdProfile.java
@@ -1,5 +1,7 @@
 package wepaht.profile;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,6 +9,9 @@ import wepaht.domain.User;
 import wepaht.repository.UserRepository;
 
 import javax.annotation.PostConstruct;
+import org.apache.log4j.Logger;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import wepaht.Application;
 
 @Configuration
 @Profile(value = "prod")
@@ -15,6 +20,8 @@ public class ProdProfile {
     @Autowired
     private UserRepository userRepository;
 
+    Logger log = Logger.getLogger(Application.class.getName());
+
     @PostConstruct
     public void init() {
         User admin = new User();
@@ -22,5 +29,37 @@ public class ProdProfile {
         admin.setUsername("admin");
         admin.setPassword("admin");
         userRepository.save(admin);
+    }
+
+    public DataSource prodDataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        log.info("Initializing postgreSQL database");
+
+        URI dbUri;
+
+        try {
+            dbUri = new URI(databaseUrl);
+
+        } catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":"
+                + dbUri.getPort() + dbUri.getPath();
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+
+        return dataSource;
     }
 }
